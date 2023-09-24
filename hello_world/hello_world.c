6a99a97cbf231bb90a6f633954602b54b25f95ba
Jacqueline Chao <jacqc2102@gmail.com>
2023-09-24 09:19:48 -0400

    Add hello_world project

---
+++ hello_world/hello_world.c
@@ -0,0 +1,6 @@
+// Include the stdio (Standard Input and Output) C library (A C library is a structured collection of other files)
+#include <stdio.h> // The stdio library provides the printf() function
+
+int main(void) {
+    printf("Hello, world!");
+}
